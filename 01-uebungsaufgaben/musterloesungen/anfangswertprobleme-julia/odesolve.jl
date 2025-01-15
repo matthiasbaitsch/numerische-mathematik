87bf87179c1e1beef2aaad6e751b55bcec1c52eb
Matthias Baitsch <matthias.baitsch@me.com>
2025-01-15 13:01:26 +0100

    .x

--- 01-uebungsaufgaben/musterloesungen/anfangswertprobleme-julia/odesolve.jl
+++ 01-uebungsaufgaben/musterloesungen/anfangswertprobleme-julia/odesolve.jl
@@ -17,7 +17,9 @@ function halbschritt(F, I, h, y0)
     for x = I[1]:h:I[2]
         push!(xs, x)
         push!(ys, y)
-        y += h * F(x, y)
+        k1 = F(x, y)
+        k2 = F(x + h / 2, y + h / 2 * k1)
+        y += h * k2
     end
     return xs, stack(ys, dims=1)
 end
@@ -29,7 +31,11 @@ function rungekutta(F, I, h, y0)
     for x = I[1]:h:I[2]
         push!(xs, x)
         push!(ys, y)
-        y += h * F(x, y)
+        k1 = F(x, y)
+        k2 = F(x + h / 2, y + h / 2 * k1)
+        k3 = F(x + h / 2, y + h / 2 * k2)
+        k4 = F(x + h, y + h * k3) 
+        y = y + h / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
     end
     return xs, stack(ys, dims=1)
 end
--- 01-uebungsaufgaben/musterloesungen/anfangswertprobleme-python/odesolve.py
+++ 01-uebungsaufgaben/musterloesungen/anfangswertprobleme-python/odesolve.py
@@ -8,7 +8,7 @@ def euler(F, I, h, y0):
         xs.append(x)
         ys.append(y)
         y = y + h * F(x, y)
-    return xs, np.array(ys)
+    return np.array(xs), np.array(ys)
 
 def halbschritt(F, I, h, y0):
     y = y0
@@ -17,8 +17,10 @@ def halbschritt(F, I, h, y0):
     for x in np.arange(I[0], I[1], h):
         xs.append(x)
         ys.append(y)
-        y = y + h * F(x, y)
-    return xs, np.array(ys)
+        k1 = F(x, y)
+        k2 = F(x + h / 2, y + h / 2 * k1)
+        y = y + h * k2
+    return np.array(xs), np.array(ys)
 
 def rungekutta(F, I, h, y0):
     y = y0
@@ -27,5 +29,9 @@ def rungekutta(F, I, h, y0):
     for x in np.arange(I[0], I[1], h):
         xs.append(x)
         ys.append(y)
-        y = y + h * F(x, y)
-    return xs, np.array(ys)
+        k1 = F(x, y)
+        k2 = F(x + h / 2, y + h / 2 * k1)
+        k3 = F(x + h / 2, y + h / 2 * k2)
+        k4 = F(x + h, y + h * k3) 
+        y = y + h / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
+    return np.array(xs), np.array(ys)
